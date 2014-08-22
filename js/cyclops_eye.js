84979fbd8560f3b944bb776f8a0e8d325d78e9b6
hamfz <hfancherr@gmail.com>
2014-08-21 20:28:07 -0500

    updated js to be a bit cleaner

--- js/cyclops_eye.js
+++ js/cyclops_eye.js
@@ -1,27 +1,31 @@
+var $w = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidth = function() {
+        w = $w.width();
+        h = $w.height();
+    };
+
 var eye = $("#eye");
 var pupil = $("#pupil");
 
 var e_radius = parseInt(eye.attr("r"));
 var p_radius = parseInt(pupil.attr("r"));
 
-
-
+//function to move pupil within eye based on mouse cursor
 function move_pupil(mouse_x,mouse_y){
 	
 	eye_pos = eye.position();
 	svg = $("#svg1");
 	svg_pos = svg.position();
-
-
 	
-cx = eye_pos.left+svg_pos.left+57;
+	cx = eye_pos.left+svg_pos.left+57;
 	cy = eye_pos.top+svg_pos.top+57;
 
 
 	dx = cx + e_radius - mouse_x;
 	dy = cy + e_radius - mouse_y;
 	R = Math.min(Math.sqrt(dx*dx+dy*dy),e_radius-p_radius);
-
 	angle = Math.atan2(dy,dx);
 
 	x = e_radius - p_radius - (R*Math.cos(angle));
@@ -30,31 +34,17 @@ cx = eye_pos.left+svg_pos.left+57;
 	xa = cx - x;
 	ya = cy - y;
 
-
-
 	pupil.attr("cx",x+parseInt(eye.attr("cx"))-57);
 	pupil.attr("cy",y+parseInt(eye.attr("cy"))-57);
 
-
-
-
-
 }
-
 
 $(window).on("mousemove",function(event){
 	move_pupil(event.pageX,event.pageY);
 });
 
-var $win = $(window),
-    w = 0,h = 0,
-    rgb = [],
-    getWidth = function() {
-        w = $win.width();
-        h = $win.height();
-    };
-
-$win.resize(getWidth).mousemove(function(e) {
+//recolor background as mouse moves
+$w.resize(getWidth).mousemove(function(e) {
     
     rgb = [
         Math.round(e.pageX/w * 255),
@@ -65,3 +55,4 @@ $win.resize(getWidth).mousemove(function(e) {
     $(document.body).css('background','rgb('+rgb.join(',')+')');
     
 }).resize();
+
